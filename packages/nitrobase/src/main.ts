c40670587b16bbf81e5e2449be30578054abbfbe
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-28 17:24:00 +0330

    refactor(nitrobase): update `exports` & remove extra files
    Co-authored-by: Ali Mihandoost <ali.mihandoost@gmail.com>

--- packages/nitrobase/src/main.ts
+++ packages/nitrobase/src/main.ts
@@ -1,5 +1,6 @@
 import {packageTracer} from '@alwatr/nanolib';
 
-export * from '@alwatr/nitrobase-types';
+export * from '@alwatr/nitrobase-engine';
+export * from '@alwatr/nitrobase-reference';
 
 __dev_mode__: packageTracer.add(__package_name__, __package_version__);
