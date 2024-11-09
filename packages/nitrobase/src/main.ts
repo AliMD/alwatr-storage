aa779bee0913b0d6e48eac88906ab893b2a1e513
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-11-09 11:38:20 +0330

    feat(nitrobase): export `nitrobase-client` from main module

--- packages/nitrobase/src/main.ts
+++ packages/nitrobase/src/main.ts
@@ -5,5 +5,6 @@ export * from '@alwatr/nitrobase-reference';
 export * from '@alwatr/nitrobase-helper';
 export * from '@alwatr/nitrobase-types';
 export * from '@alwatr/nitrobase-user-management';
+export * from '@alwatr/nitrobase-client';
 
 __dev_mode__: packageTracer.add(__package_name__, __package_version__);
